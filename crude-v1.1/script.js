c3af4b5ae1ce9caadfeeea67d592918a25656b13
LeticiaCuero <leticia.cuero06@gmail.com>
2026-01-20 12:33:34 -0300

    update version 1.1

--- crude-v1.1/script.js
+++ crude-v1.1/script.js
@@ -2,17 +2,17 @@ const users = [];
 const userList = document.getElementById("userList");
 
 function renderUsers() {
-    userList.innerHTML = '';
+    userList.innerHTML = '<h3 class="h3">Usuários:</h3>';
 
     users.forEach((name, index) => {
         const userRow = document.createElement("div");
         userRow.className = "listItem";
         userRow.innerHTML = `
-        <span id="name-${index}" class="flex-grow-1">${name}</span>
-        <div class="d-flex gap-2">
-        <button class="btn btn-sm btn-warning" onclick="editUser(${index})">Editar</button>
-        <button class="btn btn-sm btn-danger" onclick="deleteUser(${index})">Excluir</button>
-        </div>
+            <span id="name-${index}" class="listName">${name}</span>
+            <div class="listAction">
+                <button class="btn-edit" onclick="editUser(${index})">Editar</button>
+                <button class="btn-delete" onclick="deleteUser(${index})">Excluir</button>
+            </div>
         `;
         userList.appendChild(userRow);
     });
@@ -26,7 +26,6 @@ function addUser() {
         users.push(newName);
         input.value = '';
         renderUsers();
-        showAlert('alert-add');
     }
 }
 
@@ -35,23 +34,12 @@ function editUser(index) {
     const currentName = users[index];
 
     nameSpan.innerHTML = `
-    <input type="text" id="editInput-${index}" class="form-control form-control-sm" value="${currentName}">
+    <input type="text" id="editInput-${index}" class="formUser" value="${currentName}">
     `;
     const actions = nameSpan.nextElementSibling;
     actions.innerHTML = `
-    <button class="btn btn-sm btn-success mt-1" onclick="saveEdit(${index})">Salvar</button>
+    <button class="btn-primary" onclick="saveEdit(${index})">Salvar</button>
     `;
-}
-
-function showAlert(targetId) {
-    const alertBox = document.querySelector('.alert');
-    ['alert-add', 'alert-edit', 'alert-delete'].forEach(id => {
-        document.getElementById(id).classList.toggle('d-none', id !== targetId);
-    });
-    alertBox.classList.remove('d-none');
-    setTimeout(() => {
-        alertBox.classList.add('d-none');
-    }, 2000);
 }
 
 function saveEdit(index) {
@@ -59,14 +47,12 @@ function saveEdit(index) {
     if (newName !== '') {
         users[index] = newName;
         renderUsers();
-        showAlert('alert-edit');
     }
 }
 
 function deleteUser(index) {
     users.splice(index, 1);
     renderUsers();
-    showAlert('alert-delete');
 }
-renderUsers();
 
+renderUsers();
